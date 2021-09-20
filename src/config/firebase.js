84b6cecc0a19b2190cb9df90c2cfc394154fe43f
ashutosh <ashutosh.tripathi@walmart.com>
2021-09-20 10:44:16 +0530

    updated envs

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,14 +2,13 @@ require('dotenv').config()
 
 
 console.log("process.env.API_KEY,", process.env,)
-console.log("process.env.API_KEY,", process.env.API_KEY,)
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.APP_ID,
 };
 
